035c658a1d2adbddfb261effe24d93bafdec72d4
Alexandre Tranchant <alexandre.tranchant@gmail.com>
2017-08-07 23:12:09 +0200

    New unit tests.

--- src/AppBundle/Bean/Log.php
+++ src/AppBundle/Bean/Log.php
@@ -35,12 +35,12 @@ class Log
     /**
      * @var int Version of the entity.
      */
-    private $version;
+    private $version = 0;
 
     /**
      * @var string update or create
      */
-    private $action;
+    private $action = '';
 
     /**
      * @var DateTime Date time of log
@@ -50,12 +50,22 @@ class Log
     /**
      * @var string username
      */
-    private $username;
+    private $username = '';
 
     /**
      * @var array data
      */
     private $data = [];
+
+    /**
+     * Has Log a version.
+     *
+     * @return bool
+     */
+    public function hasVersion(): bool
+    {
+        return !empty($this->version);
+    }
 
     /**
      * Getter of version.
@@ -71,10 +81,12 @@ class Log
      * Setter of version.
      *
      * @param int $version
+     * @return Log
      */
     public function setVersion(int $version)
     {
         $this->version = $version;
+        return $this;
     }
 
     /**
@@ -91,10 +103,12 @@ class Log
      * Setter of action.
      *
      * @param string $action
+     * @return Log
      */
     public function setAction(string $action)
     {
         $this->action = $action;
+        return $this;
     }
 
     /**
@@ -111,10 +125,22 @@ class Log
      * Setter of Log time.
      *
      * @param DateTime $logged
+     * @return Log
      */
     public function setLogged(DateTime $logged)
     {
         $this->logged = $logged;
+        return $this;
+    }
+
+    /**
+     * Is Log time initialized?
+     *
+     * @return bool
+     */
+    public function isLogged()
+    {
+        return $this->logged instanceof DateTime;
     }
 
     /**
@@ -131,6 +157,7 @@ class Log
      * Setter of username.
      *
      * @param string $username
+     * @return Log
      */
     public function setUsername(string $username = null)
     {
@@ -139,6 +166,7 @@ class Log
         }else{
             $this->username = $username;
         }
+        return $this;
     }
 
     /**
@@ -155,10 +183,12 @@ class Log
      * Setter of data.
      *
      * @param array $data
+     * @return Log
      */
     public function setData(array $data)
     {
         $this->data = $data;
+        return $this;
     }
 
 }
---
+++ src/AppBundle/Tests/unit/Bean/LogTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * This file is part of the GEquip Application.
+ *
+ * PHP version 7.1
+ *
+ * (c) Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ *
+ * @category  Testing
+ *
+ * @author    Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @copyright 2017 Alexandre Tranchant
+ * @license   GNU General Public License, version 3
+ *
+ * @link      http://opensource.org/licenses/GPL-3.0
+ */
+
+namespace AppBundle\Tests\Bean;
+
+use AppBundle\Bean\Log;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Log Bean test case.
+ *
+ * @category Testing
+ *
+ * @author   Alexandre Tranchant <alexandre.tranchant@gmail.com>
+ * @license  GNU General Public License, version 3
+ *
+ * @link     http://opensource.org/licenses/GPL-3.0
+ */
+class LogTest extends TestCase
+{
+    /**
+     * @var Log
+     */
+    private $log;
+
+    /**
+     * Setup before each unit test.
+     */
+    protected function setUp()
+    {
+        $this->log = new Log();
+    }
+
+    /**
+     * Testing Base::constructor.
+     */
+    public function testConstructor()
+    {
+        //No null value for a bean
+        self::assertNotNull($this->log->getAction());
+        self::assertNotNull($this->log->getUsername());
+        self::assertFalse($this->log->isLogged());
+        self::assertFalse($this->log->hasVersion());
+
+        //Default value test
+        self::assertEmpty($this->log->getVersion());
+        self::assertEmpty($this->log->getAction());
+        self::assertEmpty($this->log->getUsername());
+        self::assertCount(0, $this->log->getData());
+    }
+
+    /**
+     * Testing getter and setter of Version.
+     */
+    public function testVersion()
+    {
+        self::assertFalse($this->log->hasVersion());
+        $expected = 42;
+        self::assertEquals($this->log, $this->log->setVersion($expected));
+        self::assertEquals($expected, $this->log->getVersion());
+        self::assertTrue($this->log->hasVersion());
+
+        $expected = 0;
+        self::assertEquals($this->log, $this->log->setVersion($expected));
+        self::assertEquals($expected, $this->log->getVersion());
+        self::assertFalse($this->log->hasVersion());
+    }
+
+    /**
+     * Testing getter and setter of Action.
+     */
+    public function testAction()
+    {
+        $expected = 'action';
+        self::assertEquals($this->log, $this->log->setAction($expected));
+        self::assertEquals($expected, $this->log->getAction());
+    }
+
+    /**
+     * Testing getter and setter of Username.
+     */
+    public function testUsername()
+    {
+        $expected = 'username';
+        self::assertEquals($this->log, $this->log->setUsername($expected));
+        self::assertEquals($expected, $this->log->getUsername());
+
+        $expected = '';
+        self::assertEquals($this->log, $this->log->setUsername(null));
+        self::assertEquals($expected, $this->log->getUsername());
+    }
+
+    /**
+     * Testing getter and setter of Logged.
+     */
+    public function testLogged()
+    {
+        self::assertFalse($this->log->isLogged());
+        $expected = new \DateTime();
+        self::assertEquals($this->log, $this->log->setLogged($expected));
+        self::assertEquals($expected, $this->log->getLogged());
+        self::asserttrue($this->log->isLogged());
+    }
+
+}
